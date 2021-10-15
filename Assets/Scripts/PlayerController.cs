5392f5155bad159674795aa46e8af05b18f18304
Ross Alexander <rossaalexander@gmail.com>
2021-10-15 12:46:26 +0200

    Add player jump animation and default to running animation on game start

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rb;
     private bool _grounded;
+    private Animator _playerAnimator;
+
     [SerializeField] private float jumpForce;
     public bool gameOver;
 
@@ -11,6 +13,7 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _playerAnimator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -21,6 +24,7 @@ public class PlayerController : MonoBehaviour
     private void Jump()
     {
         if (!Input.GetKeyDown(KeyCode.Space) || !_grounded) return;
+        _playerAnimator.SetTrigger("Jump_trig");
         _grounded = false;
         _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
@@ -30,6 +34,7 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Ground"))
         {
             _grounded = true;
+            
         }
         else if (other.gameObject.CompareTag("Obstacle"))
         {
