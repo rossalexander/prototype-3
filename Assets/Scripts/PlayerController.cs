ff79b655a8f8bb5e6b953a059e0660c80f6785c1
Ross Alexander <rossaalexander@gmail.com>
2021-10-15 13:31:38 +0200

    Play music, jump, and crash sounds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,14 @@ public class PlayerController : MonoBehaviour
     private Rigidbody _rb;
     private bool _grounded;
     private Animator _playerAnimator;
+    private AudioSource _playerAudio;
+
     [SerializeField] private ParticleSystem impactParticle;
     [SerializeField] private ParticleSystem dirtParticle;
-
     [SerializeField] private float jumpForce;
+    [SerializeField] private AudioClip jumpSound;
+    [SerializeField] private AudioClip crashSound;
+
     public bool gameOver;
 
 
@@ -16,6 +20,7 @@ public class PlayerController : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody>();
         _playerAnimator = GetComponent<Animator>();
+        _playerAudio = GetComponent<AudioSource>();
     }
 
     private void Update()
@@ -30,6 +35,7 @@ public class PlayerController : MonoBehaviour
         _playerAnimator.SetTrigger("Jump_trig");
         _grounded = false;
         _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        _playerAudio.PlayOneShot(jumpSound, 1f);
         dirtParticle.Stop();
     }
 
@@ -47,9 +53,8 @@ public class PlayerController : MonoBehaviour
             dirtParticle.Stop();
             impactParticle.Play();
             _playerAnimator.SetBool("Death_b", true);
+            _playerAudio.PlayOneShot(crashSound, 1f);
             _playerAnimator.SetInteger("DeathType_int", 1);
-            
-            
         }
     }
 }
